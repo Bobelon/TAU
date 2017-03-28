917fad579a68b7a0e66e3fac79d92bba32a9908a
Filip Wspaniały <Filip Wspaniay>
2017-03-28 11:17:09 +0200

    poprawiony pom.xml do jbehave

---
+++ lab5/src/test/java/pl/edu/pjwstk/lab5/WebTest.java
@@ -0,0 +1,46 @@
+package pl.edu.pjwstk.lab5;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTest {
+	
+	private static WebDriver driver;
+	WebElement element;
+
+	@BeforeClass
+	public static void driverSetup() {
+		// ChromeDrirver, FireforxDriver, ...
+		System.setProperty("webdriver.chrome.driver", "..\\ChromeDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void homePage(){
+		driver.get("http://localhost/TIN/Projekt/Kod/src/");
+		
+		element = driver.findElement(By.linkText("BigBang"));
+		assertNotNull(element);
+	}
+
+	@AfterClass
+	public static void cleanp() {
+		driver.quit();
+	}
+}
